eb530cd9d2573f78fd19e1ffb4c945d2edb1c66d
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2019-01-30 18:35:19 +0000

    expect 'keys' prop so that we can remove refs when inputs unmount

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import * as React from "react";
 type Props = {
   onFocus?: () => mixed,
   onBlur?: () => mixed,
+  keys?: [],
   children: ({ onFocus: mixed }) => mixed
 };
 
 class MergeFocus extends React.Component<Props> {
-  state = {
-    elements: []
-  };
+  // we don't need to rerender when we get new refs, so no need to store in state
+  // (in fact, triggering a rerender causes an infinite loop)
+  elements = {};
 
   static defaultProps = {
+    keys: [],
     onFocus: undefined,
     onBlur: undefined
   };
 
   // is the given element one of the elements that we're merging
   isMerged(element) {
-    const { elements } = this.state;
     // we could traverse up the DOM as per https://stackoverflow.com/a/38019906/1582783
     // or maybe if thisComponentRef.contains(e.relatedTarget)
     // but this wouldn't work e.g with portals
@@ -30,13 +31,16 @@ class MergeFocus extends React.Component<Props> {
     // Could the context api be used to detect child-of?
     // if we could 'get react component associated with this event' then there may be an option but that feels like an anti pattern
     // ^^ think about this anti pattern and why!
-    // console.log(this.state.elements);
-    // console.log(e.relatedTarget);
-    return elements.indexOf(element) !== -1;
-    // e.currentTarget.contains(e.relatedTarget)
+    // if it was the component in question we could pass down onBlur/onChange, however it's the target of a blur event that we need to check against to prevent it from firing, so we don't know what this is until the user causes a blur - and even then we only have a DOM reference to it
+    // if we passed refs using createRef rather than a function then we could use ref.current
+    // TODO: consider refactoring to use createRef
+    // https://reactjs.org/docs/refs-and-the-dom.html
+    return Object.keys(this.elements).some(
+      key => this.elements[key] === element
+    );
   }
 
-  handleFocusChange(e) {
+  handleFocusChange(key, e) {
     const { onFocus } = this.props;
     // Don't focus if e.relatedTarget (where the user came from) is included in group
     if (!this.isMerged(e.relatedTarget)) {
@@ -44,7 +48,7 @@ class MergeFocus extends React.Component<Props> {
     }
   }
 
-  handleBlurChange(e) {
+  handleBlurChange(key, e) {
     const { onBlur } = this.props;
     // Don't blur if e.relatedTarget (where the user is moving to) is included in group
     if (!this.isMerged(e.relatedTarget)) {
@@ -52,26 +56,30 @@ class MergeFocus extends React.Component<Props> {
     }
   }
 
-  handleRef(input) {
-    // TODO: how to handle refs being removed? Need a unit test
-    input &&
-      this.setState(prevState =>
-        prevState.elements.indexOf(input) === -1
-          ? {
-              elements: prevState.elements.concat([input])
-            }
-          : null
-      );
+  handleRef(key, element) {
+    // TODO: Need a unit test for refs being removed
+    if (element) {
+      this.elements[key] = element;
+    } else {
+      delete this.elements[key];
+    }
   }
 
   render() {
-    const { children } = this.props;
+    const { children, keys } = this.props;
 
-    return children({
-      onFocus: e => this.handleFocusChange(e),
-      onBlur: e => this.handleBlurChange(e),
-      ref: input => this.handleRef(input)
-    });
+    const inputs = keys.reduce(
+      (acc, key) => ({
+        [key]: {
+          onFocus: e => this.handleFocusChange(key, e),
+          onBlur: e => this.handleBlurChange(key, e),
+          ref: input => this.handleRef(key, input)
+        },
+        ...acc
+      }),
+      {}
+    );
+    return children(inputs);
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -8,28 +8,28 @@ import MergeFocus from ".";
 
 afterEach(cleanup);
 
-it("works", () => {
+it("merges focus and blur events", () => {
   const blur = jest.fn();
   const focus = jest.fn();
 
   const { getByLabelText, getByTitle, getByText } = render(
     <form title="Form">
-      <MergeFocus onBlur={blur} onFocus={focus}>
-        {mergeFocus => (
+      <MergeFocus keys={["day", "month", "year"]} onBlur={blur} onFocus={focus}>
+        {inputs => (
           <>
             <div>
               <label>
-                Day: <input name="day" {...mergeFocus} />
+                Day: <input name="day" {...inputs.day} />
               </label>
             </div>
             <div>
               <label>
-                Month: <input name="month" {...mergeFocus} />
+                Month: <input name="month" {...inputs.month} />
               </label>
             </div>
             <div>
               <label>
-                Year: <input name="year" {...mergeFocus} />
+                Year: <input name="year" {...inputs.year} />
               </label>
             </div>
           </>
@@ -45,6 +45,8 @@ it("works", () => {
   const form = getByTitle("Form");
   const button = getByText("click me");
 
+  expect(focus).toHaveBeenCalledTimes(0);
+
   userEvent.click(day);
   userEvent.type(day, "10");
   userEvent.click(month);
@@ -53,12 +55,118 @@ it("works", () => {
   userEvent.type(year, "12");
 
   expect(form).toHaveFormValues({ day: "10", month: "11", year: "12" });
+
   expect(focus).toHaveBeenCalledTimes(1);
   expect(blur).toHaveBeenCalledTimes(0);
 
   userEvent.click(button);
 
   expect(blur).toHaveBeenCalledTimes(1);
+});
+
+it("handles refs being removed", () => {
+  const blur = jest.fn();
+  const focus = jest.fn();
+
+  class TestForm extends React.Component {
+    state = {
+      showMonth: true
+    };
+
+    render() {
+      const { showMonth } = this.state;
+
+      return (
+        <form title="Form">
+          <MergeFocus
+            keys={["day", "month", "year"]}
+            onBlur={blur}
+            onFocus={focus}
+          >
+            {inputs => (
+              <>
+                <div>
+                  <label>
+                    Day: <input name="day" {...inputs.day} />
+                  </label>
+                </div>
+                {showMonth && (
+                  <div>
+                    <label>
+                      Month: <input name="month" {...inputs.month} />
+                    </label>
+                  </div>
+                )}
+                <div>
+                  <label>
+                    Year: <input name="year" {...inputs.year} />
+                  </label>
+                </div>
+              </>
+            )}
+          </MergeFocus>
+          <button type="button">click me</button>
+          <button
+            type="button"
+            onClick={e => this.setState({ showMonth: false })}
+          >
+            hide month
+          </button>
+        </form>
+      );
+    }
+  }
+
+  const { getByLabelText, getByTitle, getByText } = render(<TestForm />);
+
+  const day = getByLabelText("Day:");
+  const month = getByLabelText("Month:");
+  const year = getByLabelText("Year:");
+  const form = getByTitle("Form");
+  const button = getByText("click me");
+  const hideMonth = getByText("hide month");
+
+  expect(focus).toHaveBeenCalledTimes(0);
+
+  userEvent.click(day);
+  userEvent.type(day, "10");
+  userEvent.click(month);
+  userEvent.type(month, "11");
+  userEvent.click(year);
+  userEvent.type(year, "12");
+
+  expect(form).toHaveFormValues({ day: "10", month: "11", year: "12" });
+
+  expect(focus).toHaveBeenCalledTimes(1);
+  expect(blur).toHaveBeenCalledTimes(0);
+
+  userEvent.click(button);
+
+  expect(blur).toHaveBeenCalledTimes(1);
+
+  userEvent.click(hideMonth);
+
+  // TODO: does this ensure month doesn't exiset? Check toHaveFormValues spec
+  expect(form).toHaveFormValues({ day: "10", year: "12" });
+  expect(form).not.toHaveFormValues({ month: "11" });
+
+  userEvent.click(day);
+  userEvent.type(day, "20");
+
+  // debugOn = true;
+  userEvent.click(year);
+  // debugOn = false;
+
+  userEvent.type(year, "22");
+
+  expect(form).toHaveFormValues({ day: "20", year: "22" });
+
+  expect(focus).toHaveBeenCalledTimes(2);
+  expect(blur).toHaveBeenCalledTimes(1);
+
+  userEvent.click(button);
+
+  expect(blur).toHaveBeenCalledTimes(2);
 });
 
 // TODO: merge onBlur onFocus and ref functions with existing usage
